1f2d761a6527800d7ebf5a6dfcc5b298494f674f
Kushang Shah <kushangshah131@gmail.com>
2020-06-01 23:26:29 +0530

    Email sending

--- Check/userCheck.php
+++ Check/userCheck.php
@@ -61,7 +61,38 @@ if(isset($_SESSION['signup_login'])){
             
             $query1 = "INSERT INTO users (u_name, real_name, email, u_password, vkey, verified, gender, theme) VALUES( '$u_name','$name', '$email', '$password', '$vkey', '0','$gender', '$theme')";
             
-    
+            if($conn->query($query1)){
+                $_SESSION['signup_r'] = "yes";
+                
+                $subject = "ToConnect: Verify your Email Address";
+                $headers  = 'MIME-Version: 1.0' . "\r\n";
+                $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+ 
+                // Creating email headers
+                $headers .= 'X-Mailer: PHP/' . phpversion();
+                
+                $message = '<html><body>';
+                $message .= '<h1 style="color:black;">Hi '.$name.'!</h1>';
+                $message .= '<p style="color:green;font-size:19px;">Thanks for registration '.$name.'<br></p>';
+                $message .= '<p style="color:black;font-size:16px;">ToConnect has sent you a link to verify your email address.<br></p>';
+                $message .= '<a style=\"font-size:16px;\" href="https://keltagoodlife.co/ToConnect/verify?vkey='.$vkey.'" >Click this link to verify your email</a>';
+                $message .= '<p style="color:black;font-size:16px;">This is a software generated email, so do not reply to this email</p>';
+                $message .= '<p style="color:black;font-size:16px;">Thank you<br><br>Regards,<br>Team ToConnect</p>';
+                
+                $message .= '</body></html>';
+                
+                mail($email,$subject,$message,$headers);
+                
+            }
+            else{
+                echo "Something went wrong"; 
+            }
+            
+        }
+        else{
+            echo "This email address is already registered";
+        }
+        
 }
 else{
     echo "Signup first";
