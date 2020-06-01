dc5d325fac68d9966c00225dd44ede995495cd04
Kushang Shah <kushangshah131@gmail.com>
2020-06-01 23:35:30 +0530

    final index.js

--- Js/index.js
+++ Js/index.js
@@ -89,3 +89,103 @@ function OpenLogIn()
 		}
 	    
 	}
+let SignUpCheck = () => {
+		
+		let formS = document.getElementById('signup');
+		document.getElementById('name').name = "Name";
+		document.getElementById('email').name = "Email";
+		document.getElementById('password1').name = "Pass1";
+		document.getElementById('password2').name = "Pass2";
+		document.getElementById('gender').name = "Gender";
+		let check_data = new Check();
+		let error = document.getElementById('SignUpError');
+		let name = formS.Name.value;
+		let email = formS.Email.value;
+		let pass = formS.Pass1.value;
+		let passCon = formS.Pass2.value;
+		let gender = formS.Gender.value;
+	
+		if(name == "" || email == "" || pass == "" || passCon == "" || gender == ""){
+			alert("Please enter every details");
+			error.innerHTML = "Please enter every details";
+			return false;
+		}
+		
+		if(name.length <= 4) {
+			alert("Please enter your full name");
+			error.innerHTML = "Please enter your full name";
+			return false;
+		}
+		
+		try{
+			if(!check_data.emailCheck(email, error)){
+				
+				return false;
+				
+			}
+		
+			if(check_data.check(email)){
+				alert("Please enter valid email");
+				error.innerHTML = "Please enter valid email";
+				return false;
+			}
+			
+			if(check_data.check(name)){
+				alert("Please enter valid name");
+				error.innerHTML = "Please enter valid email";
+				return false;
+			}
+			
+			if(check_data.check(pass)){
+				alert("Please enter valid password");
+				error.innerHTML = "Please enter valid email";
+				return false;
+			}
+			
+			if(check_data.check(passCon)){
+				alert("Please enter valid password");
+				error.innerHTML = "Please enter valid email";
+				return false;
+			}
+		}
+		catch(err){
+			alert("There is a problem: "+err);
+			return;
+		}
+		
+		if(pass.length <= 5){
+			alert("password must be more than 5 letters");
+			error.innerHTML = "password must be more than 5 letters";
+			return false;
+		}
+		
+		if(pass !== passCon){
+			alert("Both passwords do not match");
+			error.innerHTML = "Both passwords do not match";
+			return false;
+		}
+	
+		let str = "Name="+name+"&Email="+email+"&Pass="+pass+"&Gender="+gender;
+    	let xhttp = new XMLHttpRequest();
+    	let loader = document.getElementById('loaderSignUp');
+    		xhttp.onreadystatechange = function() {
+    			loader.style.display = "block";
+    			if(this.readyState == 4 && this.status == 200){
+    				error.innerHTML = this.responseText;
+    				loader.style.display = "none";
+    				if(this.responseText == ""){
+    				    
+    				    formS.action = "logins";
+    					formS.method = "post";
+    					formS.submit();
+    					
+    				}
+    			}
+		    }
+		xhttp.open("POST", "Check/userCheck", true);
+		xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		xhttp.send(str);
+	
+	}
+	
+	
