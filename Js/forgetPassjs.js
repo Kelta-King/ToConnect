e0ecaa045b9fbb0cf898c657ac139fd48d701421
Kushang Shah <kushangshah131@gmail.com>
2020-06-03 13:01:39 +0530

    checking email and other inputs

--- Js/forgetPassjs.js
+++ Js/forgetPassjs.js
@@ -22,4 +22,24 @@ let send = () => {
 		error.innerHTML = "Please enter valid email";
         return false;
     }
+    
+    let str = "email="+email;
+    let xhttp = new XMLHttpRequest();
+    let loader = document.getElementById('loader');
+    
+    xhttp.onreadystatechange = function() {
+    	loader.style.display = "block";
+    	if(this.readyState == 4 && this.status == 200){
+    		error.innerHTML = this.responseText;
+    		loader.style.display = "none";
+    		if(this.responseText == ""){
+    		    alert("Email sent");
+    		    location.reload(); 
+     		}
+    	}
+    }
+    xhttp.open("POST", "sendrecovery", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send(str);
+    
 }
