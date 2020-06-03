9881dc37bf1fa89911acf89a5f9333f8bd21d354
Kushang Shah <kushangshah131@gmail.com>
2020-06-03 13:02:23 +0530

    Checking and updating new pass

--- Js/backupemail.js
+++ Js/backupemail.js
@@ -17,4 +17,24 @@ let send = () => {
 		error.innerHTML = "Please enter valid email";
         return false;
     }
+    
+    let str = "data=<?php echo $_REQUEST['data'] ?>"+"&pass="+pass;
+    let xhttp = new XMLHttpRequest();
+    let loader = document.getElementById('loader');
+    
+    xhttp.onreadystatechange = function() {
+    	loader.style.display = "block";
+    	if(this.readyState == 4 && this.status == 200){
+    		error.innerHTML = this.responseText;
+    		loader.style.display = "none";
+    		if(this.responseText.length == 1){
+    		    alert("Password changed");
+    		    location.replace("https://keltagoodlife.co/ToConnect"); 
+     		}
+    	}
+    }
+    xhttp.open("POST", "dorecovery", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send(str);
+    
 }
