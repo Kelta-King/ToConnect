6e371874f8effb174278efaab49b914a77c57a36
Kushang Shah <kushangshah131@gmail.com>
2020-06-03 12:51:50 +0530

    login after verify login

---
+++ login.php
@@ -0,0 +1,23 @@
+<?php
+
+session_start();
+
+if(isset($_SESSION['signup_login']) && isset($_SESSION['login_user_connect'])){
+ 
+    $url = base64_decode($_SESSION['login_user_connect']);
+    
+    $url = explode("&", $url);
+    $id = $url[0];
+    $name = $url[2];
+    
+    $url = base64_encode($id."&".$name);
+    
+    header("Location:Users/profile?name=".$url);
+    
+}
+else{
+    echo "<script>alert('Login first')</script>";
+    header("Location:logout");
+}
+
+?>
