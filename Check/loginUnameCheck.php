f2e623a9d6d0357a1c8a6d575ce04fae203da9ff
Kushang Shah <kushangshah131@gmail.com>
2020-06-03 06:56:48 +0530

    Databse interaction

--- Check/loginUnameCheck.php
+++ Check/loginUnameCheck.php
@@ -9,6 +9,37 @@ if(isset($_SESSION['signup_login'])){
     $query = "SELECT `u_id`, `u_password`, `email`, `u_name` FROM `users` WHERE u_name = '$u_name'";
     
     if($data = $conn->query($query)){
+        
+        if($data->num_rows <= 0){
+            echo "Incorrect username";
+        }
+        else if($data->num_rows == 1){
+            
+            $result = $data->fetch_assoc();
+            $dbpass = $result['u_password'];
+            
+            if(password_verify($pass, $dbpass)){
+                
+                $opt = [
+                    'cost' => 12,  
+                ];
+                
+                $id = $result['u_id'];
+                $name = $result['u_name'];
+                $email = $result['email'];
+                $url1 = base64_encode($id."&".$email."&".$name);
+                $_SESSION['login_user_connect'] = $url1;
+                
+            }
+            else{
+                echo "Incorrect password";
+            }
+            
+        }
+        else{
+            echo "Incorrect details";
+        }
+        
     }
     else{
         echo "Something went wrong";
@@ -20,8 +51,9 @@ if(isset($_SESSION['signup_login'])){
 
 }
 else{
-    echo "<script>alert('Login first')</script>";
     header("Location:logout");
+    echo "login first";
+    
 }
 
 ?>
