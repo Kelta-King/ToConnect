39ed21a10b12b5773d40e9d4d7fd6a85dc3bd019
Kushang Shah <kushangshah131@gmail.com>
2020-06-03 06:52:36 +0530

    Checking from the database

--- Check/loginEmailCheck.php
+++ Check/loginEmailCheck.php
@@ -10,9 +10,39 @@ if(isset($_SESSION['signup_login'])){
     require_once("../../../Database/dbconnect_connect.php");
     
     $query = "SELECT `u_id`, `u_password`, `u_name` FROM `users` WHERE email = '$email'";
-
-    if($conn->query($query)){
-        //email check login
+    
+    if($data = $conn->query($query)){
+        
+        if($data->num_rows <= 0){
+            echo "Incorrect email";
+        }
+        else if($data->num_rows == 1){
+            
+            $result = $data->fetch_assoc();
+            $dbpass = $result['u_password'];
+            
+            if(password_verify($pass, $dbpass)){
+                
+                $opt = [
+                    'cost' => 12,  
+                ];
+                
+                $id = $result['u_id'];
+                $name = $result['u_name'];
+                
+                $url1 = base64_encode($id."&".$email."&".$name);
+                $_SESSION['login_user_connect'] = $url1;
+                
+            }
+            else{
+                echo "Incorrect password";
+            }
+            
+        }
+        else{
+            echo "Incorrect details";
+        }
+        
     }
     else{
         echo "Something went wrong";
@@ -20,6 +50,7 @@ if(isset($_SESSION['signup_login'])){
     
     $conn->close();
 
+}
 else{
     echo "<script>alert('Login first')</script>";
     header("Location:logout");
