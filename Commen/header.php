3e7ccbc833ab930913c425603a29255d204f854e
Kushang Shah <kushangshah131@gmail.com>
2020-06-05 14:48:08 +0530

    Update header.php

--- Commen/header.php
+++ Commen/header.php
@@ -2,12 +2,21 @@
 <html>
 <head>
 <title><?php echo TITLE ?></title>
+<script>
+    function myFunction() {
+      var x = document.getElementById("Demo");
+      if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+      } else { 
+        x.className = x.className.replace(" w3-show", "");
+      }
+    }
+</script>
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link href="https://fonts.googleapis.com/css2?family=Literata&display=swap" rel="stylesheet"> 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="../CSS/kel.css">
-<style>
-</style>
+
 <body>
 
 <header class="w3-bar w3-<?php echo $theme_color ?> w3-padding">
