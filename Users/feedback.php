cf6e5486f03607fd7a54482d60c94aa5505aa0ee
Kushang Shah <kushangshah131@gmail.com>
2020-06-06 09:23:16 +0530

    Update feedback.php

--- Users/feedback.php
+++ Users/feedback.php
@@ -1,1 +1,195 @@
+<?php
 
+session_start();
+
+if(isset($_SESSION['login_user_connect']) && isset($_REQUEST['name'])){
+    
+    $dts = explode("&",base64_decode($_SESSION['login_user_connect']));
+    $id = $dts[0];
+    $email = $dts[1];
+    $name = $dts[2];
+    
+    require_once("../../../Database/dbconnect_connect.php");
+    
+    $theme_color = "blue";
+    
+    $qry = "SELECT theme FROM users WHERE u_id = $id";
+    
+    if($data = $conn->query($qry)){
+        
+        $result = $data->fetch_assoc();
+        $theme_color = $result['theme'];
+        
+    }
+    
+    $q = "SELECT verified FROM users WHERE u_id = $id";
+    
+    if($d = $conn->query($q)){
+        
+        $rslt = $d->fetch_assoc();
+        
+        if($rslt['verified'] == '1'){
+            
+            define("TITLE", "feedback | ToConnect");
+            include("../Commen/header.php");
+?>
+<!-- Verified account area -->
+<div class="w3-modal" id="editun">
+    <div class="w3-modal-content w3-animate-zoom w3-card-4" style="max-width:500px">
+      <header class="w3-container w3-<?php echo $theme_color ?>"> 
+        <span onclick="document.getElementById('editun').style.display='none'" 
+        class="w3-button w3-display-topright w3-xlarge">&times;</span>
+        <h2>My Account</h2>
+      </header>
+      <form class="w3-container">
+<?php
+    $query1 = "SELECT u_name FROM users WHERE u_id = $id";
+    
+    if($data1 = $conn->query($query1)){
+        
+        $result = $data1->fetch_assoc();
+?>
+        <div class="w3-container w3-margin w3-padding w3-large">
+            <center>
+        		<div class="w3-text-red" id="color-error"></div>
+        		<div class="loader" id="color-loader" style="display:none"></div>
+            </center>
+            <b>Choose Theme:</b> <span class="w3-badge w3-blue w3-text-blue w3-margin-left kel-hover-2" onclick="updatetheme('blue')">a</span>
+            <span class="w3-badge w3-green kel-hover-2 w3-text-green" onclick="updatetheme('green')">a</span>
+            <span class="w3-badge w3-red kel-hover-2 w3-text-red" onclick="updatetheme('red')">a</span>
+            <span class="w3-badge w3-pink kel-hover-2 w3-text-pink" onclick="updatetheme('pink')">a</span>
+            <span class="w3-badge w3-indigo kel-hover-2 w3-text-indigo" onclick="updatetheme('indigo')">a</span>
+        </div>
+        <hr>
+        <center>
+        	<div class="w3-text-red" id="up-error"></div>
+        	<div class="loader" id="up-loader" style="display:none"></div>
+        </center>
+        <table class="w3-table w3-margin-top">
+            <tr>
+                <td class="w3-large" style="text-align:right;vertical-align: middle;">Username: </td>
+                <td><input class="w3-border w3-input" id="u_name" value="<?php echo $result['u_name'] ?>"></td>
+            </tr>
+        
+             <tr style="margin-top:30px;">
+                <td class="w3-large" style="text-align:right;vertical-align: middle;">Current Pass: </td>
+                <td><input class="w3-border w3-input" id="pass" type="password" placeholder="Current Password"></td>
+            </tr>
+            <tr>
+                <td class="w3-large" style="text-align:right;vertical-align: middle;">New Pass: </td>
+                <td><input class="w3-border w3-input" id="newPas" type="password" placeholder="New Password"></td>
+            </tr>
+            <tr>
+                <td class="w3-large" style="text-align:right;vertical-align: middle;">Again new Pass: </td>
+                <td><input class="w3-border w3-input" id="newAga" type="password" placeholder="New Password Again"></td>
+            </tr> 
+            
+        </table>
+<?php
+    }
+    else{
+        echo "something went wrong";
+    }
+?>
+        <hr>
+        <button type="button" class="w3-button w3-right w3-margin-bottom w3-margin-left w3-border w3-round w3-<?php echo $theme_color ?>" onclick="updateProfile()"><i class="fa fa-pencil-square-o"></i> Update</button>
+        <button type="button" onclick="document.getElementById('editun').style.display='none'" class="w3-button w3-right w3-margin-bottom w3-border w3-round"><i class="fa fa-times"></i> Cancel</button>
+        
+      </form>
+      
+    </div>
+</div>
+
+<center>
+<div class="w3-content w3-padding-32 w3-animate-bottom">
+    <div class="w3-white w3-padding-32 w3-light-gray">
+        <div class="w3-xxlarge">FeedBack</div>
+        <hr style="border-bottom:2px solid black" class="w3-margin">
+<div class="w3-padding" style="text-align:left">
+Please fill the below form to provide us the honest response of your opinion regarding to ToConnect we application. Your response do matters for us to improve the site and services. Thank you :)
+</div>
+<form>
+    <center>
+    <div class="loader" id="loader" style="display:none"></div>
+    <div class="w3-text-red" id="error"></div>
+    </center>
+<div class="w3-section w3-margin" style="text-align:left">
+    <b>Name</b>
+    <input class="w3-input w3-border" name="name" id="name">
+</div>
+<div class="w3-section w3-margin" style="text-align:left">
+    <b>Message</b>
+    <textarea class="w3-input w3-border" name="msg" id="msg"></textarea>
+</div>
+<div class="w3-section w3-margin" style="text-align:left">
+    <button type="button" onclick="sendfeedback()" class="w3-button w3-<?php echo $theme_color ?> kel-hover-2">Send</button>
+</div>
+
+</form>
+
+    </div>
+</div>
+</center>
+<script>
+
+let sendfeedback = () => {
+    
+    let name = document.getElementById('name').value;
+    let msg = document.getElementById('msg').value;
+    
+    if(name == "" || msg == ""){
+        
+        alert("Please fill the data");
+        return;
+        
+    }
+    
+    let str = "name="+name+"&msg="+msg;
+    let xhttp = new XMLHttpRequest();
+    let loader = document.getElementById('loader');
+    let error = document.getElementById('error');
+    
+    xhttp.onreadystatechange = function() {
+    	loader.style.display = "block";
+    	if(this.readyState == 4 && this.status == 200){
+    		error.innerHTML = this.responseText;
+    		loader.style.display = "none";
+    		if(this.responseText == ""){
+    		    alert("Feedback sent");
+    		    location.reload(); 
+     		}
+    	}
+    }
+    xhttp.open("POST", "sendfeedback", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send(str);
+    
+}
+
+</script>
+<script src="Js/varified.js"></script>
+</body>
+</html>
+<?php
+        }
+        else{
+            //non varified account
+            header("Location:../logout"); 
+        }
+        
+    }
+    else{
+        echo "Something went wrong, Please try again later"; 
+    }
+    
+    
+?>
+<?php
+    
+    $conn->close();
+}
+else{
+    header("Location:../logout"); 
+}
+
+?>
