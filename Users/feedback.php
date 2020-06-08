9c3f68c17622fd1d470ca73bfb531b8ee688f99f
Kushang Shah <kushangshah131@gmail.com>
2020-06-08 12:11:14 +0530

    Update feedback.php

--- Users/feedback.php
+++ Users/feedback.php
@@ -130,43 +130,7 @@ Please fill the below form to provide us the honest response of your opinion reg
     </div>
 </div>
 </center>
-<script>
-
-let sendfeedback = () => {
-    
-    let name = document.getElementById('name').value;
-    let msg = document.getElementById('msg').value;
-    
-    if(name == "" || msg == ""){
-        
-        alert("Please fill the data");
-        return;
-        
-    }
-    
-    let str = "name="+name+"&msg="+msg;
-    let xhttp = new XMLHttpRequest();
-    let loader = document.getElementById('loader');
-    let error = document.getElementById('error');
-    
-    xhttp.onreadystatechange = function() {
-    	loader.style.display = "block";
-    	if(this.readyState == 4 && this.status == 200){
-    		error.innerHTML = this.responseText;
-    		loader.style.display = "none";
-    		if(this.responseText == ""){
-    		    alert("Feedback sent");
-    		    location.reload(); 
-     		}
-    	}
-    }
-    xhttp.open("POST", "sendfeedback", true);
-    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhttp.send(str);
-    
-}
-
-</script>
+<script src="Js/feedback.js"></script>
 <script src="Js/varified.js"></script>
 </body>
 </html>
